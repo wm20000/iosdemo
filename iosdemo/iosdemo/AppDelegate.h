c1a4fd7dc4eaf97a78b0f581867f440596f40297
wangmo <wm20000@sina.com>
2015-10-30 11:58:18 +0800

    init

---
+++ iosdemo/iosdemo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iosdemo
+//
+//  Created by wangmo on 10/30/15.
+//  Copyright © 2015 wmios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ iosdemo/iosdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosdemo
+//
+//  Created by wangmo on 10/30/15.
+//  Copyright © 2015 wmios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
