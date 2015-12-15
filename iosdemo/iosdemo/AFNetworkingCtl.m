59a4026046be60a7250113e1647c0860808b2a61
wangmo <wm20000@sina.com>
2015-12-15 11:20:35 +0800

    ...

--- iosdemo/iosdemo/AFNetworkingCtl.m
+++ iosdemo/iosdemo/AFNetworkingCtl.m
@@ -28,11 +28,29 @@
 /**
  加载网络图片
  */
-- (IBAction)AFNetworkingImageStart:(id)sender {
+- (IBAction)AFNetworkingImageTest:(id)sender {
     
     [self.myImageView setImageWithURL:[NSURL URLWithString:@"http://7xnjxd.com1.z0.glb.clouddn.com/bigimg2.jpg"] placeholderImage:[UIImage imageNamed:@"icon_default"]];
 }
 
+- (IBAction)AFNGetJSON:(id)sender {
+    
+    self.myTextView.text = @"";
+    
+    /*
+     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+     [manager GET:@"http://7xkme5.com1.z0.glb.clouddn.com/movies2.json" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+     //        NSLog(@"JSON: %@", responseObject);
+     for (NSDictionary *item in responseObject) {
+     self.myTextView.text = [self.myTextView.text stringByAppendingString:[item valueForKeyPath:@"title"]];
+     self.myTextView.text = [self.myTextView.text stringByAppendingString:[item valueForKeyPath:@"url"]];
+     }
+     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+     //        NSLog(@"Error: %@", error);
+     self.myTextView.text = error.localizedDescription;
+     }];
+     */
+}
 /**
  Post
  没有实现真的Post
@@ -40,6 +58,7 @@
 - (IBAction)AFNPost:(id)sender {
     
     self.myTextView.text = @"";
+    /*
     
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     NSDictionary *parameters = @{@"name":@"wm"};
@@ -49,26 +68,7 @@
 //        NSLog(@"Error: %@", error);
         self.myTextView.text = error.localizedDescription;
     }];
-}
-
-/**
- Get解析JSON
- */
-- (IBAction)AFNGetJSON:(id)sender {
-    
-    self.myTextView.text = @"";
-    
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    [manager GET:@"http://7xkme5.com1.z0.glb.clouddn.com/movies2.json" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        NSLog(@"JSON: %@", responseObject);
-        for (NSDictionary *item in responseObject) {
-            self.myTextView.text = [self.myTextView.text stringByAppendingString:[item valueForKeyPath:@"title"]];
-            self.myTextView.text = [self.myTextView.text stringByAppendingString:[item valueForKeyPath:@"url"]];
-        }
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-//        NSLog(@"Error: %@", error);
-        self.myTextView.text = error.localizedDescription;
-    }];
+     */
 }
 
 @end
